56ca0a3506a5f50f9be83c1aa89b0e47e8a97709
fmarcos38 <fmarcos_23@hotmail.com>
2023-12-05 17:43:40 -0300

    docker arch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ProductModule } from './products/product.module';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
