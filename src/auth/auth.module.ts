791bf8976805eeb191dda249a4bcde7afa449f21
fmarcos38 <fmarcos_23@hotmail.com>
2023-12-18 10:15:44 -0300

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,30 +6,33 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './controllers/auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import config  from '../config';
+import { ConfigType } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
-    /* JwtModule.register({ //con este modulo el secret lo lee desde aca
-          secret: 'pepe',
+    JwtModule.register({ //con este modulo el secret lo lee desde aca
+          secret: 'marcos',
           signOptions: {
             expiresIn: '10d',
           },
-    }), */
+    }),
     //con este modulo el secret lo lee desde el config
-    JwtModule.registerAsync({
-      useFactory: () => {
+    /* JwtModule.registerAsync({
+      inject: [config.KEY],
+      useFactory: (configiService: ConfigType<typeof config>) => {
         return {
-          secret: config().mongo.jwtSecret,
+          secret: configiService.mongo.jwtSecret,
           signOptions: {
             expiresIn: '10d',
           },
         };
       }
-    }),
+    }), */
   ], //me traigo el userModule --> para usar sus metodos desde el authService
-  providers: [AuthService, LocalStrategy, ], 
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { AuthService } from '../services/auth.service';
+import { User } from 'src/Users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private authSerice: AuthService){}
 
     @UseGuards(AuthGuard('local')) //local es el nombre de la estrategia, en este punto ES el gurad el q hace toda la logica del login
     @Post('login')
     login(@Req() req: Request){
-        return req.user;
+        const user = req.user as User; //el user lo devuelve el guard y lo tipeo como User
+        return this.authSerice.generateJWT(user);
     }
 }
 
